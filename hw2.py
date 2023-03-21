2d1e73bf9ba71e20058a19007de4efce10135876
leejaemin1375 <126881963+leejaemin1375@users.noreply.github.com>
2023-03-21 23:06:59 +0900

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+def get_integer(prompt) :
+    res = int(input(prompt))
+    return res;
+
+def exchange(res) :
+    n500 = int(res/500)
+    n100 = int((res-(500*n500))/100)
+    n50 = int((res-(500*n500)-(100*n100))/50)
+    n10 = int((res-(500*n500)-(100*n100)-(50*n50))/10)
+    print('500원 동전의 개수 : ', n500)
+    print('100원 동전의 개수 : ', n100)
+    print('50원 동전의 개수 : ', n50)
+    print('10원 동전의 개수 : ', n10)
+    
+res = get_integer('동전으로 교환하고자 하는 금액은?')
+exchange(res)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
