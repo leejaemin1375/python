58b23b5b0585d29d55639916825cc2ae35439496
leejaemin1375 <126881963+leejaemin1375@users.noreply.github.com>
2023-04-17 23:44:04 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,13 @@
+for i in range(1,10):
+    for n in range(2,6) :
+        print(n,'x',i,'=', n*i, end = '\t')
+    print()
+        
+print('\t')
+
+for i in range(1,10):
+    for n in range(6,10) :
+        print(n,'x',i,'=', n*i, end = '\t')
+    print()
+
+
